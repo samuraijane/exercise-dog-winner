5384f046ea80de70b553441adadb735a5b15677f
Matthew Day <matt@matthewday.net>
2022-02-26 16:48:44 -0500

    implement dynamic routing and show a question based on the route

--- src/sectioning/main.jsx
+++ src/sectioning/main.jsx
@@ -1,9 +1,12 @@
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Landing from "../views/landing";
 
 const Main = () => {
   return (
     <main className="y-wrap">
-      <Landing />
+      <Routes>
+        <Route path="/*" element={<Landing />} />
+      </Routes>
     </main>
   )
 };
