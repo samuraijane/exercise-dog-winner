8a413e956e65de8d0df16e9a9416b9a764429476
Matthew Day <matt@matthewday.net>
2022-02-27 08:32:47 -0500

    reset images to initial state when new game begins

--- src/redux/reducers/images.js
+++ src/redux/reducers/images.js
@@ -1,10 +1,12 @@
-import { SET_IMAGE } from '../actionTypes';
+import { SET_IMAGE, SET_INITIAL_STATE } from '../actionTypes';
 
 const initialState = [];
 
 function imagesReducer(state=initialState, action) {
   if (action.type === SET_IMAGE) {
     return [...state, action.imageURL];
+  } else if (action.type === SET_INITIAL_STATE) {
+    return initialState;
   }
   return state;
 }
